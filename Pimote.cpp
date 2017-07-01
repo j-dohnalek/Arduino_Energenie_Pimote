6e4e51f49b2e598836ee948a777006aee56a0df4
Jiri Dohnalek <thinkabout3e@googlemail.com>
2017-07-01 09:39:15 +0100

    Update Pimote.cpp

--- Pimote.cpp
+++ Pimote.cpp
@@ -1,8 +1,22 @@
-/*
-  Pimote.h - Conversion for Energenie Pimote from Raspberry Pi to
-  Arduino.
-  Copyright (c) 2016 Jiri Dohnalek.  All right reserved.
-*/
+/**
+ *
+ * Controling the Energenie Pimote sockets from any Arduino based board
+ * Copyright (C) 2017  Jiri Dohnalek
+ * 
+ * File: Pimote.h
+ * 
+ * Pimote.h is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * Pimote.h is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 
 
 #include "Arduino.h" // include core Arduino function
