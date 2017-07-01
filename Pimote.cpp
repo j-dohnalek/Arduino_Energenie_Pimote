df4ad90585fc2655f90b4a3cd2e162f8893006af
Jiri Dohnalek <thinkabout3e@googlemail.com>
2017-07-01 09:40:27 +0100

    Update Pimote.cpp

--- Pimote.cpp
+++ Pimote.cpp
@@ -3,13 +3,13 @@
  * Controling the Energenie Pimote sockets from any Arduino based board
  * Copyright (C) 2017  Jiri Dohnalek
  * 
- * File: Pimote.h
+ * File: Pimote.cpp
  * 
- * Pimote.h is free software: you can redistribute it and/or modify
+ * Pimote.cpp is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * Pimote.h is distributed in the hope that it will be useful,
+ * Pimote.cpp is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
