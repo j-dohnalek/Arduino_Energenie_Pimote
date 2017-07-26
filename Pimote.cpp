4deb3d6dfaaf02cdfa2b7f6ef44d680a8dcd6fd1
Jiri Dohnalek <j.dohnalek@outlook.com>
2017-07-26 21:50:02 +0100

    Fix bugs

--- Pimote.cpp
+++ Pimote.cpp
@@ -2,9 +2,9 @@
  *
  * Controling the Energenie Pimote sockets from any Arduino based board
  * Copyright (C) 2017  Jiri Dohnalek
- * 
+ *
  * File: Pimote.cpp
- * 
+ *
  * Pimote.cpp is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
@@ -15,47 +15,51 @@
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
-
 
 #include "Arduino.h" // include core Arduino function
 #include "Pimote.h"  // include Pimote definition
 
 // Public Methods //////////////////////////////////////////////////////////////
 
-void setESD0(int p){
+void Pimote::setESD0(int p){
   // Encoded Signal D0
   gpio17 = p;
 }
 
-void setESD1(int p){
+
+void Pimote::setESD1(int p){
   // Encoder Signal D1
   gpio22 = p;
 }
 
-void setESD2(int p){
+
+void Pimote::setESD2(int p){
   // Encoder Signal D2
   gpio23 = p;
 }
 
-void setESD3(int p){
+
+void Pimote::setESD3(int p){
   // Encoder Signal D3
   gpio27 = p;
 }
 
-void setMODSEL(int p){
+void Pimote::setMODSEL(int p){
   // MODSEL mode select signal (OOK/FSK)
   gpio24 = p;
 }
 
-void setCE(int p){
+
+void Pimote::setCE(int p){
   // CE modular enable (Output ON/OFF)
   gpio25 = p;
 }
 
+
 // Enable all pins
-void Pimote::begin(void)
+void Pimote::begin()
 {
   int pin[] = {gpio17, gpio22, gpio23, gpio27, gpio24, gpio25};
   // Set the pins to output
@@ -66,18 +70,18 @@ void Pimote::begin(void)
 }
 
 
-void Pimote::On(int s)
+void Pimote::on(int s)
 {
   if(s >= 0 && s <= 4){
-    State(s, on);
+    state(s, sck_on);
   }
 }
 
 
-void Pimote::Off(int s)
+void Pimote::off(int s)
 {
   if(s >= 0 && s <= 4){
-    State(s, off);
+    state(s, sck_off);
   }
 }
 
@@ -85,14 +89,14 @@ void Pimote::Off(int s)
 // Private Variables ///////////////////////////////////////////////////////////
 
 // Binary mapping of the output state
-char* Pimote::on[5]  = {"1011", "1111", "1110","1101", "1100"};
-char* Pimote::off[5] = {"0011", "0111", "0110","0101", "0100"};
+char *Pimote::sck_on[5]  = {"1011", "1111", "1110","1101", "1100"};
+char *Pimote::sck_off[5] = {"0011", "0111", "0110","0101", "0100"};
 
 
 // Private Methods /////////////////////////////////////////////////////////////
 
 
-void Pimote::State(int socket, char* on_or_off[])
+void Pimote::state(int socket, char* on_or_off[])
 {
   // Check the state of each pin
   int pin[] = {gpio27, gpio23, gpio22, gpio17};
--- Pimote.h
+++ Pimote.h
@@ -2,9 +2,9 @@
  *
  * Controling the Energenie Pimote sockets from any Arduino based board
  * Copyright (C) 2017  Jiri Dohnalek
- * 
+ *
  * File: Pimote.h
- * 
+ *
  * Pimote.h is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
@@ -15,8 +15,9 @@
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
+
 #ifndef Pimote_h
 #define Pimote_h
 
@@ -24,9 +25,9 @@
 class Pimote
 {
   public:
-  
+
     // Setup the sockets
-    void begin(void);
+    void begin();
     // set Encoded Signal D0
     void setESD0(int p);
     // set Encoder Signal D1
@@ -40,23 +41,23 @@ class Pimote
     // set CE modular enable (Output ON/OFF)
     void setCE(int p);
     // Turn socket on
-    void On(int s);
+    void on(int s);
     // Turn socket off
-    void Off(int s);
+    void off(int s);
 
   private:
-  
-    static char* on[];    // on bits
-    static char* off[];   // off bits
+
+    static char *sck_on[];    // on bits
+    static char *sck_off[];   // off bits
     int gpio17; // Encoded Signal D0
     int gpio22; // Encoder Signal D1
     int gpio23; // Encoder Signal D2
     int gpio27; // Encoder Signal D3
     int gpio24; // MODSEL mode select signal (OOK/FSK)
     int gpio25; // CE modular enable (Output ON/OFF)
-  
+
     // Set the state of the socket
-    void State(int socket, char* on_or_off[]);
+    void state(int socket, char* on_or_off[]);
 };
 
 
